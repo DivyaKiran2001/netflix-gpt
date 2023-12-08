90400daa935afc3aaa495edcf03451d3d6372db0
DivyaKiran2001 <divyakirantatikonda@gmail.com>
2023-12-08 16:44:15 +0530

    signup and Login forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
@@ -12,20 +16,36 @@ const Login = () => {
         />
       </div>
       <form className="w-3/12 absolute p-12 bg-black bg-opacity-80 my-36 mx-auto right-0 left-0 text-white">
-        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full  bg-gray-700"
+          />
+        )}
         <input
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full  bg-gray-700"
         />
+
         <input
           type="text"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-700"
         />
         <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
-          Sign In
+          {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
+        <p className="cursor-pointer p-4 my-4" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered ? Sign In Now"}
+        </p>
       </form>
     </div>
   );
